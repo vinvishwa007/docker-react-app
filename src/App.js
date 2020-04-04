8b4720ee9c607584a57be4f8c63a81360eaa5f72
vinvishwa007 <vinvishwa007@gmail.com>
2020-04-04 18:33:25 +0530

    added travis file, fixed tests

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Hi there trrr
+          learn react
         </a>
       </header>
     </div>
